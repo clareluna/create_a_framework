c0ee3eb3cd577fe2d1f130244709e244e0c005f6
clareluna <clarelunamonahan@gmail.com>
2015-09-18 08:14:23 -0700

    broke tests, all info in routes, going to move to .get and .post

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var http = require('http');
 var Router = require('./lib/router'); // change to actual name when published on npm
 
-var router = Router(); //document that you dont need to include new in Router call in README
+var router = Router();
 
 var server = http.createServer(Router.route).listen(3000, function(){
 	console.log('the server is successfully running on port 3000');
--- test/router_test.js
+++ test/router_test.js
@@ -1,19 +1,36 @@
 'use strict';
-var expect = require('chai').expect;
 var Router = require(__dirname + './../lib/router');
+var chai = require('chai');
+var expect = chai.expect;
+var fs = require('fs');
+var http = require('http');
+		
+var router = new Router();
 
 describe('the basic router functions', function() {
-	var testRes; 
+	console.log('before each');
 	beforeEach(function() {
-		this.router = Router();
-		testRes = {test: 'testing 1, 2, 3'};		
+		http.createServer(router.route).listen(3000, function(){
+			console.log('server running at port 3000');
+		});
+		router.get('/test', function(req, res) { //sets up get response
+			res.header(200, {'content-type': 'text/plain'});// not sure what to put in here
+			res.send('testing 1, 2, 3');
+		});
+
+		router.post('/', function(req, res) { //sets up post request
+			res.json(req);
+		});	
 	});
 
-	it('should be able to set up get request', function(){
-		var testReq = { 
-			method: 'GET', 
-			url: '/test'
-		};
+	it('should be able to respond to a get request', function(done){
+		chai.request('localhost:3000')
+			.get('/test') // do I need to include header data here?
+			.header(200, {'content-type': 'text/plain'}) //set header from routes.route
+			.send('testing 1, 2, 3') //set response from router.routes
+			.end(function(err, res){
+
+			})
 		this.router.get('/test', function(req, res) {
 			expect(res.test).to.eql('testing 1, 2, 3');
 		});
@@ -32,36 +49,36 @@ describe('the basic router functions', function() {
 	});		
 });
 
-describe('the advanced router functions', function () {
-	beforeEach(function() {
-		this.router = Router(); //start new router
-	});
+// describe('the advanced router functions', function () {
+// 	beforeEach(function() {
+// 		this.router = Router(); //start new router
+// 	});
 
-	it('should be able to add a header to a get request', function() {
-		var testReq = {
-			method: 'GET',
-			url: '/test'
-		};
-		var testRes = {'content-type': 'text/plain'};
+// 	it('should be able to add a header to a get request', function() {
+// 		var testReq = {
+// 			method: 'GET',
+// 			url: '/test'
+// 		};
+// 		// var testRes.header = {'content-type': 'text/plain'};
 	
-	//this.router.get('/test', function(req, res) {
-			//expect(res.header['Content-Type']).to.eql('text/plain'); coming back undefined in prev test
-		});
-		//this.router.route(testReq, testRes); // should I be including writeHead data in testReq?
-	});
+// 	//this.router.get('/test', function(req, res) {
+// 			//expect(res.header['Content-Type']).to.eql('text/plain'); coming back undefined in prev test
+// 		// });
+// 		//this.router.route(testReq, testRes); // should I be including writeHead data in testReq?
+// 	});
 
-	it('should be able to process post data and save the json data into req.body', function() {
-		var testReq = {
-			method: 'POST',
-			url; '/test'
-			//data? to be used as body/chunk
-		}
-		var testRes = { // what data I expect to become a 'file'
-		};
-		// this.router.post('/test', function(req, res) {
-		// expect(__dirname + './../files').to.have(__dirname + './../files/' + body + '.txt');
-		});
-	});
-});
+// 	it('should be able to process post data and save the json data into req.body', function() {
+// 		var testReq = {
+// 			method: 'POST',
+// 			url: '/test'
+// 			//data? to be used as body/chunk
+// 		}
+// 		var testRes = { // what data I expect to become a 'file'
+// 		};
+// 		// this.router.post('/test', function(req, res) {
+// 		// expect(__dirname + './../files').to.have(__dirname + './../files/' + body + '.txt');
+// 		});
+// 	});
+// // });
 
-			
+// 			
