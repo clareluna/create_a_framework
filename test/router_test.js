2a8564659fac11cfcd93f83e3fc4b5534c5de8c8
clareluna <clarelunamonahan@gmail.com>
2015-09-17 21:11:36 -0700

    initial testing working

--- test/router_test.js
+++ test/router_test.js
@@ -3,23 +3,61 @@ var expect = require('chai').expect;
 var Router = require(__dirname + './../lib/router');
 
 describe('the router', function() {
+	var testRes; 
 	beforeEach(function() {
 		this.router = Router();
-		console.log('router ' + typeof Router);
+		testRes = {test: 'testing 1, 2, 3'};		
 	});
+
 	it('should be able to set up get request', function(){
-		var testReq = { //setting up the route and method we want
+		var testReq = { 
 			method: 'GET', 
 			url: '/test'
 		};
-		var testRes = { //testing up the response we exect
-			test: 'testing 1, 2, 3'
-		};
-		console.log('testRes ' + typeof testRes);
-		console.log('testReq ' + typeof testReq);	
 		this.router.get('/test', function(req, res) {
 			expect(res.test).to.eql('testing 1, 2, 3');
 		});
-		this.router.route(testReq, testRes); //initiating the function to that req, res
+		this.router.route(testReq, testRes); 
 	});	
+
+	it('should be able to set up post request', function(){
+		var testReq = { 
+			method: 'POST', 
+			url: '/test'
+		};
+		this.router.post('/test', function(req, res) {
+			expect(res.test).to.eql('testing 1, 2, 3');
+		});
+		this.router.route(testReq, testRes); 
+	});	
+	it('should be able to set up put request', function(){
+		var testReq = { 
+			method: 'PUT', 
+			url: '/test'
+		};
+		this.router.put('/test', function(req, res) {
+			expect(res.test).to.eql('testing 1, 2, 3');
+		});
+		this.router.route(testReq, testRes); 
+	});
+	it('should be able to set up patch request', function(){
+		var testReq = { 
+			method: 'PATCH', 
+			url: '/test'
+		};
+		this.router.patch('/test', function(req, res) {
+			expect(res.test).to.eql('testing 1, 2, 3');
+		});
+		this.router.route(testReq, testRes); 
+	});	
+	it('should be able to set up delete request', function(){
+		var testReq = { 
+			method: 'DELETE', 
+			url: '/test'
+		};
+		this.router.delete('/test', function(req, res) {
+			expect(res.test).to.eql('testing 1, 2, 3');
+		});
+		this.router.route(testReq, testRes); 
+	});			
 });
